408d0f0616b354b8b603b8f6b11688f7ad77b4d4
Nikita <poikl.m246@gmail.com>
2023-04-01 12:15:16 +0500

    test db3

--- DB.py
+++ DB.py
@@ -1,1 +1,24 @@
- 
+ import os
+import psycopg2
+
+# Чтение переменных окружения
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+# Подключение к PostgreSQL
+conn = psycopg2.connect(
+    host=db_host,
+    database=db_name,
+    user=db_user,
+    password=db_password
+)
+
+# Создание таблицы
+cur = conn.cursor()
+cur.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255));")
+cur.close()
+
+# Закрытие соединения
+conn.close()
